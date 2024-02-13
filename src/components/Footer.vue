ef1b2265ebf90a3c1abafc75d9dc39e23fa91fea
k4ik <kaikleal1234@gmail.com>
2024-02-13 19:03:41 -0300

    feat: create footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,37 @@
+<template>
+    <footer class="footer">
+        <p>&copy; Amálio Pinheiro, {{ currentYear }}</p>
+    </footer>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                currentYear: new Date().getFullYear()
+        };
+  }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .footer {
+        background-color: #000;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 100px;
+
+        p, a {
+            color: #fff;
+        }
+
+        a {
+            text-decoration: none;
+            &:hover {
+                transition: .5s;
+                color: rgb(186, 186, 245);
+            }
+        }
+    }
+</style>
