a4dbd5e1eae602c8c5db34ad8d3d321f24999694
k4ik <kaikleal1234@gmail.com>
2024-02-13 19:02:39 -0300

    feat: create home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <h1>Home</h1>
-  </div>
+  <Header />
+  <Home />
+  <Map />
+  <Footer />
 </template>
 
 <script>
+  import Header from '@/components/Header.vue';
+  import Home from '@/components/Home.vue';
+  import Map from '@/components/Map.vue';
+  import Footer from '@/components/Footer.vue';
+
   export default {
-    
+    components: {
+      Header,
+      Home,
+      Map,
+      Footer
+    }
   }
 </script>
 
