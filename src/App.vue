64468db23c63ec90304850db0cb333c1fda5818f
k4ik <kaikleal1234@gmail.com>
2024-02-13 19:02:54 -0300

    feat: import fonts

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
-<script>
-import { RouterView } from 'vue-router'
-</script>
+<script></script>
 
 <template>
   <RouterView />
 </template>
 
 <style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500&display=swap');
 
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+  }
 </style>
