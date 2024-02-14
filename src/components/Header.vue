0b356ae3a6e0b68ab52ba75484eab6378f42329e
k4ik <kaikleal1234@gmail.com>
2024-02-14 12:46:18 -0300

    feat: menu hamburger

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,4 +1,12 @@
 <template>
+    <nav class="menu-mobile" v-if="showMenu">
+        <button class="close-btn" @click="closeMenu">
+            <img src="../assets/images/close.png" alt="">
+        </button>
+        <router-link to="/" class="header-link" @click="closeMenu">Escola</router-link>
+        <router-link to="/alunos" class="header-link" @click="closeMenu">Alunos</router-link>
+        <router-link to="/docentes" class="header-link" @click="closeMenu">Docentes</router-link>
+    </nav>
     <header class="header">
         <h1 class="header-logo">Amálio <span>Pinheiro</span></h1>
         <nav class="header-nav">
@@ -6,12 +14,29 @@
             <router-link to="/alunos" class="header-link">Alunos</router-link>
             <router-link to="/docentes" class="header-link">Docentes</router-link>
         </nav>
+
+        <button class="hamburger-btn" @click="openMenu">
+            <img src="../assets/images/hamburger.png" alt="">
+        </button>
     </header>
+    
 </template>
 
 <script>
     export default {
-        
+        methods: {
+            closeMenu() {
+                this.showMenu = false;
+            },
+            openMenu() {
+                this.showMenu = true;
+            }
+        },
+        data() {
+            return {
+                showMenu: false
+            }
+        }
     }
 </script>
 
@@ -32,6 +57,7 @@
             }
         }
 
+        
         .header-nav {
             display: flex;
             gap: 15px;
@@ -46,6 +72,65 @@
                     color: rgb(186, 186, 245);
                 }
             }
+        }
+
+        @media screen and (max-width: 768px) {
+            .header-nav {
+                display: none;
+            }
+        }
+        
+    }
+
+    .menu-mobile {
+        position: fixed;
+        background-color: rgba(0, 0, 0, 0.692);
+        z-index: 9;
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+
+        .header-link {
+            color: #fff;
+            text-decoration: none;
+            font-size: 1.2em;
+
+            &:hover {
+                transition: .5s;
+                color: rgb(186, 186, 245);
+            }
+        }
+    }
+    
+    .hamburger-btn {
+        display: none;
+        background: transparent;
+        border: none;
+        img {
+            width: 30px;
+            height: 35px;
+        }
+    }
+
+    @media screen and (max-width: 768px) {
+        .hamburger-btn {
+            display: block;
+        }
+    }
+
+    .close-btn {
+        position: absolute;
+        top: 5%;
+        right: 5%;
+        background: transparent;
+        border: none;
+        img {
+            width: 20px;
+            height: 20px;
         }
     }
 </style>
--- src/views/DocentesView.vue
+++ src/views/DocentesView.vue
@@ -1,46 +1,49 @@
 <template>
-    <Header />
-    <main>
-      <div class="Container"  v-for="teacher in teachers" :key="teacher.id">
-        <div class="Box">
-          <div class="ImgBox">
-            <img :src="teacher.photo" :alt="teacher.name" />
-          </div>
-          <div class="social-links">
-            <a :href="`https://github.com/${teacher.github}`" target="_blank"
-              ><img src="../assets/images/github.png" class="icone" alt="Github"
-            /></a>
-            <a :href="`https://linkedin.com/in/${teacher.linkedin}`" target="_blank"
-              ><img
-                src="../assets/images/linkedin.png"
-                class="icone"
-                alt="LinkedIn"
-            /></a>
-            <a :href="`https://instagram.com/${teacher.instagram}`" target="_blank"
-              ><img
-                src="../assets/images/instagram.png"
-                class="icone"
-                alt="Instagram"
-            /></a>
-          </div>
-          <div class="img-descricao">
-            <h3>{{ teacher.name }}</h3>
-            <span>{{ teacher.profession }}</span>
-          </div>
-          
+  <Header />
+  <main>
+    <div class="Container" v-for="teacher in teachers" :key="teacher.id">
+      <div class="Box">
+        <div class="ImgBox">
+          <img :src="teacher.photo" :alt="teacher.name" />
         </div>
-        <section class="informacoes-gerais">
-          <h1>Informações Gerais</h1>
-          <ul>
-            <li>Residência: {{ teacher.residence }}</li>
-            <li>Estado Civil: {{ teacher.marital_status }}</li>
-            <li>Profissão: {{ teacher.profession }}</li>
-          </ul>
-        </section>
+        <div class="social-links">
+          <a :href="`https://github.com/${teacher.github}`" target="_blank"
+            ><img src="../assets/images/github.png" class="icone" alt="Github"
+          /></a>
+          <a
+            :href="`https://linkedin.com/in/${teacher.linkedin}`"
+            target="_blank"
+            ><img
+              src="../assets/images/linkedin.png"
+              class="icone"
+              alt="LinkedIn"
+          /></a>
+          <a
+            :href="`https://instagram.com/${teacher.instagram}`"
+            target="_blank"
+            ><img
+              src="../assets/images/instagram.png"
+              class="icone"
+              alt="Instagram"
+          /></a>
+        </div>
+        <div class="img-descricao">
+          <h3>{{ teacher.name }}</h3>
+          <span>{{ teacher.profession }}</span>
+        </div>
       </div>
-    </main>
-    <Footer />
-  </template>
+      <section class="informacoes-gerais">
+        <h1>Informações Gerais</h1>
+        <ul>
+          <li>Residência: {{ teacher.residence }}</li>
+          <li>Estado Civil: {{ teacher.marital_status }}</li>
+          <li>Profissão: {{ teacher.profession }}</li>
+        </ul>
+      </section>
+    </div>
+  </main>
+  <Footer />
+</template>
   
   
   <script>
@@ -80,89 +83,88 @@ export default {
     
   
 <style scoped>
-
 main {
   height: 650px;
   margin: 20px 0;
 }
 .Container {
-position: relative;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-flex-wrap: wrap;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
 }
 
 .Container .Box {
-position: relative;
-max-width: 20rem;
-height: 20rem;
-margin: 3rem;
+  position: relative;
+  max-width: 20rem;
+  height: 20rem;
+  margin: 3rem;
 }
 
 .ImgBox {
-position: relative;
-top: 0;
-left: 0;
-width: 100%;
-height: 100%;
-z-index: 2;
-transition: all 0.5s ease-in-out;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 2;
+  transition: all 0.5s ease-in-out;
 }
 
 .ImgBox img {
-width: 20rem;
-height: 20rem;
-object-fit: cover;
-resize: both;
+  width: 20rem;
+  height: 20rem;
+  object-fit: cover;
+  resize: both;
 }
 
 .social-links a {
-margin-left: 10px;
+  margin-left: 10px;
 }
 
 .Container .Box:hover .ImgBox {
-transform: translate(3.5, 3.5);
+  transform: translate(3.5, 3.5);
 }
 
 .Container .Box:hover .ImgBox {
-transform: translate(-1.5rem, -1.5rem);
+  transform: translate(-1.5rem, -1.5rem);
 }
 
 .icone {
-position: relative;
-top: 100%;
-left: 50%;
-transform: translateX(-50%);
-opacity: 0;
-transition: opacity 0.3s ease-in-out;
-z-index: 1;
-width: 10%;
+  position: relative;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+  z-index: 1;
+  width: 10%;
 }
 
 .icone img {
-margin-bottom: 20px;
+  margin-bottom: 20px;
 }
 
 .Box:hover .icone {
-opacity: 1;
+  opacity: 1;
 }
 
 .informacoes-gerais h1 {
-text-align: center;
-margin-top: 90px;
-margin-bottom: 20px;
+  text-align: center;
+  margin-top: 90px;
+  margin-bottom: 20px;
 }
 
 .informacoes-gerais ul {
-display: grid;
-grid-template-columns: repeat(3, 1fr);
-text-align: center;
-list-style: none;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  text-align: center;
+  list-style: none;
 }
 
 .informacoes-gerais li {
-margin-bottom: 10px;
+  margin-bottom: 10px;
 }
 </style>
